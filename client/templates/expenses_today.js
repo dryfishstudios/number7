ec5c1282e45ef9eadd233f878a8aadf6ca3dff09
Arvind Ravi <arvind.ravi@aol.in>
2014-11-01 02:46:35 +0530

    added totalExpensesToday and fixed sort

--- client/templates/expenses_today.js
+++ client/templates/expenses_today.js
@@ -1,9 +1,18 @@
 Template.expenses_today.helpers({
 	expenses: function() {
-		return Expenses.find({}, {date: -1});
+		return Expenses.find({}, {sort: {parsedDate: -1}});
 	},
-	totalExpenseToday: function() {
-		return 18;
+	totalExpensesToday: function() {
+		var expenses = Expenses.find({});
+		var totalExpensesToday = 0;
+		expenses.forEach(function(expense) {
+			var todaysDate = new Date();
+			if(expense.date.setHours(0,0,0,0) == todaysDate.setHours(0,0,0,0)) {
+				totalExpensesToday += parseInt(expense.cost);
+			}
+		});
+		console.log(totalExpensesToday);
+		return totalExpensesToday.toFixed(2);
 	},
 	dailyAllowance: function() {
 		return 25;
@@ -13,17 +22,14 @@ Template.expenses_today.helpers({
 Template.expenses_today.events({
 
 	'click #addNewExpense': function(event, template) {
-
 		event.preventDefault();
-
+		
 		$('#addNewExpenseOverlay').removeClass('animated zoomOut');
 		$('#expenses_today_overlay').addClass('animated zoomOut');
 		$("#addNewExpenseOverlay").css({
 			'display': 'block'
 		});
 		$('#addNewExpenseOverlay').addClass('animated zoomIn');
-		
-		
 	},
 
 	'click #closeAddNewExpense': function(event, template) {
@@ -45,14 +51,29 @@ Template.expenses_today.events({
 			cost  = event.target.cost.value,
 			location = event.target.location.value,
 			category = event.target.category.value,
-			date = Date.parse(new Date());
+			date = new Date(),
+			parsedDate = Date.parse(date);
 			
 		Expenses.insert({
 			title: title,
-			cost: cost,
+			cost: cost.toFixed(2),
 			location: location,
 			category: category,
-			date: date
+			date: date,
+			parsedDate: parsedDate
 		});
+		
+		$('#addNewExpenseOverlay').removeClass('animated zoomIn');
+		$("#addNewExpenseOverlay").css({
+			'display': 'none'
+		});
+		$('#expenses_today_overlay').removeClass('animated zoomOut');
+		$('#expenses_today_overlay').addClass('animated zoomIn');
+		
+		// Clear fields
+		$('#title').val('');
+		$('#cost').val('');
+		$('#location').val('');
+		$('#category').val('');
 	}
 })
