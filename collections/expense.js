b1fd05f617a98043b67168ddc2ea1348a4589314
Arvind Ravi <arvind.ravi@aol.in>
2014-10-31 16:25:22 +0530

    added collections

---
+++ collections/expense.js
@@ -0,0 +1,1 @@
+var Expense = Meteor.collection('expense');
--- server/server.js
+++ server/server.js
@@ -1,5 +1,13 @@
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
-}
+Meteor.startup(function () {
+    if(!Expense.find({}).count()) {
+    	var title = 'DummyExpense';
+		var cost = 50;
+		var location = 'TheMall';
+		var category = 'Malls';
+		Expense.insert({title: title, cost: cost, location: location, category: category});
+    }
+	if(!Category.find({}).count()) {
+		var name = 'Malls';
+		Category.insert({name: name});
+	}
+});
