d7112acedc357a47e57780b55fa5fa3d8b440d88
Prabhu <prabhu.saitu@gmail.com>
2014-10-31 21:56:11 +0530

    some w

--- client/templates/expenses_today.js
+++ client/templates/expenses_today.js
@@ -2,4 +2,14 @@ Template.expenses_today.helpers({
 	expenses: function() {
 		return Expenses.find({});
 	}
+});
+
+Template.expenses_today.events({
+
+	'click #addNewExpense': function(event, template) {
+
+		event.preventDefault();
+
+		console.log("clicked add new expense");
+	}
 })
